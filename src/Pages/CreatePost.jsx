192c2715ec996265d7976dc567efdedefc6ec426
Shahnas <shahanasp9@gmail.com>
2023-06-06 06:03:34 +0530

    mast:dall-e-clone

---
+++ src/Pages/CreatePost.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { FormField, Loader } from "../Components";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+
+function CreatePost() {
+  return <div>CreatePost</div>;
+}
+
+export default CreatePost;
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+//utils folder to store reusable functions
